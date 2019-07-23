6eb7d5ca6786b56877d5dc51447e5464d214d0f3
Liu <liuyue05@baidu.com>
2019-07-23 15:14:58 +0800

    Release ignore

--- build/Release/.deps/Release/obj.target/binding/src/srqc-nodejs.o.d
+++ build/Release/.deps/Release/obj.target/binding/src/srqc-nodejs.o.d
@@ -1,3 +1,15 @@
 cmd_Release/obj.target/binding/src/srqc-nodejs.o := c++ '-DNODE_GYP_MODULE_NAME=binding' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/liuyue05/Library/Caches/node-gyp/11.2.0/include/node -I/Users/liuyue05/Library/Caches/node-gyp/11.2.0/src -I/Users/liuyue05/Library/Caches/node-gyp/11.2.0/deps/openssl/config -I/Users/liuyue05/Library/Caches/node-gyp/11.2.0/deps/openssl/openssl/include -I/Users/liuyue05/Library/Caches/node-gyp/11.2.0/deps/uv/include -I/Users/liuyue05/Library/Caches/node-gyp/11.2.0/deps/zlib -I/Users/liuyue05/Library/Caches/node-gyp/11.2.0/deps/v8/include  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/binding/src/srqc-nodejs.o.d.raw   -c -o Release/obj.target/binding/src/srqc-nodejs.o ../src/srqc-nodejs.cc
-Release/obj.target/binding/src/srqc-nodejs.o: ../src/srqc-nodejs.cc
+Release/obj.target/binding/src/srqc-nodejs.o: ../src/srqc-nodejs.cc \
+  /Users/liuyue05/Library/Caches/node-gyp/11.2.0/include/node/node.h \
+  /Users/liuyue05/Library/Caches/node-gyp/11.2.0/include/node/v8.h \
+  /Users/liuyue05/Library/Caches/node-gyp/11.2.0/include/node/v8-version.h \
+  /Users/liuyue05/Library/Caches/node-gyp/11.2.0/include/node/v8config.h \
+  /Users/liuyue05/Library/Caches/node-gyp/11.2.0/include/node/v8-platform.h \
+  /Users/liuyue05/Library/Caches/node-gyp/11.2.0/include/node/node_version.h
 ../src/srqc-nodejs.cc:
+/Users/liuyue05/Library/Caches/node-gyp/11.2.0/include/node/node.h:
+/Users/liuyue05/Library/Caches/node-gyp/11.2.0/include/node/v8.h:
+/Users/liuyue05/Library/Caches/node-gyp/11.2.0/include/node/v8-version.h:
+/Users/liuyue05/Library/Caches/node-gyp/11.2.0/include/node/v8config.h:
+/Users/liuyue05/Library/Caches/node-gyp/11.2.0/include/node/v8-platform.h:
+/Users/liuyue05/Library/Caches/node-gyp/11.2.0/include/node/node_version.h:
